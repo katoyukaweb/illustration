2623a8091a7037d81f921b6df469892a4c203349
katoyukaweb <80556789+katoyukaweb@users.noreply.github.com>
2021-05-26 15:55:10 +0900

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+window.addEventListener('load', ()=> {
+    var elem = document.querySelector('.grid');
+    var msnry = new Masonry( elem, {
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-sizer',
+        percentPosition: true
+    });
+})
+
+
+s
